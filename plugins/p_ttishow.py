05a67367c814ee400958674307545f2d6edcf5d7
kuttanunni <94504467+kuttanunni@users.noreply.github.com>
2021-11-17 19:38:29 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('©️ Channel', url='https://t.me/EDIT_REPO')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
